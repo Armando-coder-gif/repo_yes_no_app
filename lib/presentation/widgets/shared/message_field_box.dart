d7cf648422c892f2f744aaf62427eb7b65172829
Armando Gonzalez <armando.gonzalez@correo.unimet.edu.ve>
2024-03-24 15:42:07 -0600

    comportamiento del formfield

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -5,29 +5,38 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textController = TextEditingController();
+    final focusNode = FocusNode();
+
     final outlineInputBorder = UnderlineInputBorder(
         borderSide: const BorderSide(color: Colors.transparent),
         borderRadius: BorderRadius.circular(20));
 
     final inputDecoration = InputDecoration(
+      hintText: 'Escribe un mensaje',
       enabledBorder: outlineInputBorder,
       focusedBorder: outlineInputBorder,
       filled: true,
       suffixIcon: IconButton(
         icon: const Icon(Icons.send_outlined),
         onPressed: () {
-          print('valor de la caja de texto: ');
+          final textValue = textController.value.text;
+          print('button: $textValue');
+          textController.clear();
+          focusNode.requestFocus();
         },
       ),
     );
 
     return TextFormField(
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
+      controller: textController,
       decoration: inputDecoration,
       onFieldSubmitted: (value) {
         print('submit value: $value');
-      },
-      onChanged: (value) {
-        print('changed: $value');
       },
     );
   }
